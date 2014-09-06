c68fe437f8a509b82e62731feff7b76e76a6fe22
grachro <grachro@gmail.com>
2014-09-06 15:51:19 +0900

    Initial Commit

---
+++ swift-layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-layout
+//
+//  Created by grachro on 2014/09/06.
+//  Copyright (c) 2014年 grachro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
